abb1ee0ba8625731997059cfe3080605ae585bad
patilashish312 <35179165+patilashish312@users.noreply.github.com>
2021-04-02 08:58:04 +0100

    Delete InstrumentProviderInstrumentProvider.java

--- InstrumentProviderInstrumentProvider.java
+++
@@ -1,6 +0,0 @@
-import java.util.Map;
-import java.util.Set;
-
-public interface InstrumentProvider {
-	Map<InstrumentIdentifier,Instrument> getInstruments(Set<InstrumentIdentifier> instrumentIdentifiers);	
-}
